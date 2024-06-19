13f114a0452b91b191200d3b8d5a13baade11fea
marionpoole <166074673+marionpoole@users.noreply.github.com>
2024-06-18 20:12:53 -0500

    Update RPS.js

--- unit-testing/exercises/RPS.js
+++ unit-testing/exercises/RPS.js
@@ -1,6 +1,8 @@
 function whoWon(player1,player2){
-
-   if (player1 === player2){
+if (player1 || player2 !== 'rock' || 'paper' || 'scissors'){
+  return 'Invalid response!'
+}
+  if (player1 === player2){
      return 'TIE!';
    }
    
@@ -12,9 +14,11 @@ function whoWon(player1,player2){
      return 'Player 2 wins!';
    }
    
-   if (player1 === 'scissors' && player2 === 'rock '){
+   if (player1 === 'scissors' && player2 === 'rock'){
      return 'Player 2 wins!';
-   }
- 
-   return 'Player 1 wins!';
+   } else {
+    return 'Player 1 wins!';
  }
+}
+
+ module.exports = whoWon;
