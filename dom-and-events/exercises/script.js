af228882508e06af420682ad5a5615a727647ca4
Marion Poole <mapoole09@hotmail.com>
2024-08-01 13:07:12 -0500

    Updated DOM and Events Exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,24 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener("click", function() {
+        paragraph.innerHTML = 'Houston, we have liftoff!'
+    });
+
+    missionAbort.addEventListener("mouseover", function() {
+        button.style.backgroundColor='#ff0000';
+    });
+
+    missionAbort.addEventListener("mouseout", function(event) {
+        event.target.style.backgroundColor= "";
+    });
+
+    missionAbort.addEventListener("click", function() {
+        window.confirm("Are you sure you want to abort the mission?");
+        if(true) {
+           paragraph.innerHTML = 'Mission aborted! Space shuttle returning home.'; 
+        }
+    });
 }
 
 window.addEventListener("load", init);
