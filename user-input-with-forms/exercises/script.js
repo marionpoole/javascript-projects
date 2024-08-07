c7813e4e1d126d09200c78efb9f53576422bf5a5
Marion Poole <mapoole09@hotmail.com>
2024-08-07 18:21:55 -0500

    Updated User Input with Forms Exercise

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,16 @@
 //Code Your Solution Below
+window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        let testNameInput=document.querySelector("input[name=testName]");
+        let testDateInput=document.querySelector("input[name=testDate]");
+        let rocketTypeInput=document.querySelector("input[name=rocketType]");
+        let boosterCountInput=document.querySelector("input[name=boosterCount]");
+        let windRatingInput=document.querySelector("input[name=windRating]");
+        let productionServersInput=document.querySelector("input[name=productionServers]");
+        if(testNameInput.value === "" || testDateInput.value === "" || rocketTypeInput === "*Select One*" || boosterCountInput === "" || windRatingInput === "" || productionServersInput === "off") {
+            alert("All fields are required!");
+            event.preventDefault();
+        }
+        });
+});
